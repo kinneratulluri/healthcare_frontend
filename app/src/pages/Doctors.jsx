e30f3698bd6f39172d45a6652cc27a8265bee9bf
kinnera tulluri <kinneratulluri@gmail.com>
2025-04-12 00:26:21 +0530

    style:updated signup page style

--- app/src/pages/Doctors.jsx
+++ app/src/pages/Doctors.jsx
@@ -1,22 +1,46 @@
-import {React,useEffect} from 'react'
-import axios from "axios"
-
+import { React, useEffect, useState } from 'react'
+import { ViewDetailsButton } from '../components/ui/Button.jsx';
+import { allDoctors } from '../services/doctor.service.js';
 
 function Doctors() {
-    async function getDoctors() {
-        try {
-            const apiUrl = import.meta.env.VITE_BASE_URL;
-            const response = await axios.get(`${apiUrl}/api/v1/patient/availableDoctors`)
-            console.log(response.data)
-        } catch (error) {
-            console.error("error",error)
+    //SET AND UPDATE DOCTORS DETAILS
+    const [doctors, setDoctors] = useState([])
+    //FETCH DETAILS FROM BACKEND AND ONLY RUNS ONCE AFTER COMPONENT MOUNTS
+    useEffect(() => {
+        async function getDoctors() {
+            try {
+                const response = await allDoctors()
+
+                setDoctors(response.doctors)
+                console.log(response.data)
+
+            } catch (error) {
+                console.error("error", error)
+            }
         }
-    }
-    useEffect(()=>{
+
         getDoctors()
-    },[])
+    }, [])//
     return (
-        <div>All Doctors</div>
+        <div className=" min-h-screen bg-gray-100 p-8">
+            <h1 className="text-3xl font-bold text-center mb-8 text-red-800">All Doctors</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
+                {doctors.map((doctor, index) => (
+                    <div key={index}
+                        className="bg-white shadow-md rounded-2xl p-6 transition-transform hover:scale-105 hover:shadow-2xs"
+                    >
+                        <h2 className="text-xl font-semibold text-gray-800">Name : {doctor.name}</h2>
+                        <p className="text-gray-600 mt-1"> Experience: {doctor.experience} year(s)</p>
+                        <p className="text-gray-600">Specialization: {doctor.specialization || "General"}</p>
+                        <ViewDetailsButton doctorId={doctor._id} />
+                    </div>
+
+                )
+
+                )}
+            </div>
+        </div>
+
     )
 }
 
