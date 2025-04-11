0e8a1ed7e4537e9ecfa3e00037c34cb16ea11366
kinnera tulluri <kinneratulluri@gmail.com>
2025-04-12 00:31:00 +0530

    feat:added doctorprofile,signup routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import './App.css'
-import Doctors from "./pages/doctors";
-
+import Doctors from "./pages/Doctors.jsx";
+import DoctorSignUp from "./pages/DoctorSignUp.jsx";
+import DoctorProfile from "./pages/DoctorProfile.jsx";
 function App() {
  
   return (
@@ -9,6 +10,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Doctors/>}></Route>
+      <Route path="/doctor/:id" element={<DoctorProfile/>}></Route>
+      <Route path="/signup" element={<DoctorSignUp/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
