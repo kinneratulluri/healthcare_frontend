522ac538781ad615c4ebdc42bee6f8e5020272ac
kinnera tulluri <kinneratulluri@gmail.com>
2025-04-14 12:56:10 +0530

    feat:added routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,17 +3,26 @@ import './App.css'
 import Doctors from "./pages/Doctors.jsx";
 import DoctorSignUp from "./pages/DoctorSignUp.jsx";
 import DoctorProfile from "./pages/DoctorProfile.jsx";
+import DoctorLogin from "./pages/DoctorLogin.jsx";
+import PatientSignUp from './pages/PatientSignUp.jsx';
+import PatientLogin from './pages/PatientLogin.jsx';
+import Navbar from "./components/Navbar.jsx";
+
 function App() {
- 
+
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Doctors/>}></Route>
-      <Route path="/doctor/:id" element={<DoctorProfile/>}></Route>
-      <Route path="/signup" element={<DoctorSignUp/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Doctors />}></Route>
+          <Route path="/doctor/:id" element={<DoctorProfile />}></Route>
+          <Route path="/signup" element={<DoctorSignUp />}></Route>
+          <Route path="/login" element={<DoctorLogin />} />
+          <Route path="/patient/signup" element={<PatientSignUp />} />
+          <Route path="/patient/login" element={<PatientLogin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
